cb2adacbd0645d5e292c3528a1a754b499ce9613
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-03-22 01:13:45 +0700

    add fonts to initial project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto_Slab } from "next/font/google";
+
+const RobotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={RobotoSlab.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
